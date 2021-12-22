4272fb63edd880cbaa3e4a79cd1a7ef9f4f7ac3a
Jeison David Hernandez Rayo <jeison..03@hotmail.com>
2021-12-22 08:09:45 -0500

    Primero Reto3

---
+++ src/main/java/com/scrum7/controller/ProductController.java
@@ -0,0 +1,50 @@
+package com.scrum7.controller;
+
+import com.scrum7.model.Product;
+import com.scrum7.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ * @author Jeison Hernandez
+ */
+
+
+@RestController
+@RequestMapping("/api/cleaningproduct")
+public class ProductController {
+    @Autowired
+    private ProductService servicio;
+
+    @GetMapping("/all")
+    public List<Product> listAll() {
+        return servicio.getAll();
+    }
+
+    @GetMapping("/{reference}")
+    public Optional<Product> getProduct(@PathVariable("reference") String reference) {
+        return servicio.getProduct(reference);
+    }
+
+    @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product create(@RequestBody Product product) {
+        return servicio.create(product);
+    }
+
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product update(@RequestBody Product product) {
+        return servicio.update(product);
+    }
+
+    @DeleteMapping("/{reference}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public boolean delete(@PathVariable("reference") String reference) {
+        return servicio.delete(reference);
+    }
+}
